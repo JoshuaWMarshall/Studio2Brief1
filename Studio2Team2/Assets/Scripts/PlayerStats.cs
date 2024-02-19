5e815e8f55e7f3b2c669ca7ba64a73ae56a425cc
DESKTOP-TATKLJM\joshm <1029946@student.sae.edu.au>
2024-02-19 18:48:10 +1000

    something that wasnt staged

--- Studio2Team2/Assets/Scripts/PlayerStats.cs
+++ Studio2Team2/Assets/Scripts/PlayerStats.cs
@@ -21,19 +21,26 @@ public class PlayerStats : MonoBehaviour
         playerHp = 100;
 
     }
-  
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+         if (other.tag == "Player")
+         {
+             playerXp =+ 10;
+             Debug.Log("Gained xp, " + playerXpThreshold + playerXp + " xp left to level up");
+         }
+    }
         // Update is called once per frame
         void Update()
-    {
-        // debug log is just to test if its working or not.
-        // Debug.Log("player level = " + playerLvl + " player xp threshold = " + playerXpThreshold);
-
-        if(playerHp <=0)
         {
+            // debug log is just to test if its working or not.
+            // Debug.Log("player level = " + playerLvl + " player xp threshold = " + playerXpThreshold);
 
-            playerHp= 0;
-            Gameover.SetActive(true);
-            
+            if (playerHp <= 0)
+            {
+                playerHp = 0;
+                Gameover.SetActive(true);
+            }
         }
-    }
+              
+        
 }
--- Studio2Team2/Assets/Scripts/XpPickUp.cs
+++ Studio2Team2/Assets/Scripts/XpPickUp.cs
@@ -9,19 +9,8 @@ public class XpPickUp : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // GetComponent<int>().playerXp();
+        
     }
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-       // if (other.tag == "Player")
-       // {
-       //     playerXp + 10;
-       //     Debug.Log("Gained xp, " + playerXpThreshold + playerXp + " xp left to level up");
-       // }
-    }
-
-
     // Update is called once per frame
     void Update()
     {
